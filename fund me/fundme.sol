9f74f7ce982a6c0f667f1de3a473b6586e63b98e
Maanyajha-12 <maanya_j@ch.iitr.ac.in>
2026-02-26 08:34:24 +0530

    5

--- fund me/fundme.sol
+++ fund me/fundme.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.31;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
     using PriceConverter for uint256;
